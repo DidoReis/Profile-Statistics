6661bab80ae2100b7b419d7e55287e189e076eb6
DidoReis <djdidoreis@gmail.com>
2023-03-24 14:49:49 -0300

    Refactoring

--- main.js
+++ main.js
@@ -18,24 +18,3 @@ counters.forEach((counter) => {
     }
   }
 });
-
-const countersEl = document.querySelectorAll(".counter");
-
-countersEl.forEach((counter) => {
-  counter.innerText = "0";
-  incrementCounter();
-
-  function incrementCounter() {
-    let currentNum = +counterEl.innerText;
-    const dataCeil = counterEl.getAttribute("data-ceil");
-    const increment = dataCeil / 15;
-    currentNum = Math.ceil(currentNum + increment);
-
-    if (currentNum < dataCeil) {
-      counterEl.innerText = currentNum;
-      setTimeout(incrementCounter, 50);
-    } else {
-      counterEl.innerText = dataCeil;
-    }
-  }
-});
